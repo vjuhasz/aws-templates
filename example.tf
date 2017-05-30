50ee4059099e22d863a69f243d800663464e8a54
Viktor Juhasz <viktor.juhasz@thirdbridge.com@CL-COGNO379s-MA.local>
2017-05-30 17:33:32 +0100

    Initial commit

---
+++ example.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  access_key = "AKIAJHWVOXDJNHM4FRAA"
+  secret_key = "8cELcFP0Pmw7fNPi7RiXkMnprJZywB4zgE8N1HZr"
+  region     = "eu-west-1"
+}
+
+resource "aws_security_group" "test" {
+  name = "test"
+  description = "my test SG"
+  vpc_id = "vpc-74458913"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-a8d2d7ce"
+  subnet_id = "subnet-f5f13d92"
+  security_groups = ["${aws_security_group.test.id}"]
+  instance_type = "t2.micro"
+}
